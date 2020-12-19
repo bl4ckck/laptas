acfe585bf25fc371a214560919a2feaa6d8ca824
kukuhfajar123 <kukuhfajar13@gmail.com>
2020-12-19 13:01:49 +0700

    masih sampai inisial firebase auth

--- app/src/main/java/com/kelompoklaptas/laptas/login.java
+++ app/src/main/java/com/kelompoklaptas/laptas/login.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class login extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "laptas";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void goToDetail(View view) {
@@ -21,5 +26,13 @@ public class login extends AppCompatActivity {
         String message = "Ar0EzxoBqBXjdvktIvIB";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
--- app/src/main/java/com/kelompoklaptas/laptas/register.java
+++ app/src/main/java/com/kelompoklaptas/laptas/register.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class register extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class register extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 }
